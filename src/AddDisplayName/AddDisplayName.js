36faea879441d3afb7750ed8cf4b5e514fe070c8
eugene-finn <1287175@gmail.com>
2020-03-31 19:35:27 +0300

    AddDisplayName

--- src/AddDisplayName/AddDisplayName.js
+++ src/AddDisplayName/AddDisplayName.js
@@ -4,4 +4,12 @@ import React, { Component } from 'react';
   Напишите простой HOC и укажите для него displayName
 */
 
-export const withDisplayName = () => {}
+export const withDisplayName = WrappedComponent => {
+  console.log(Component);
+  return class extends Component {
+    static displayName = `HOC${WrappedComponent.displayName || 'Component'}`;
+    render() {
+      return <WrappedComponent />;
+    }
+  };
+};
